dff8591ba92c1385b0bfbd39e87ef69f8adaf284
Teller501 <ande777j@stud.kea.dk>
2022-12-13 16:38:16 +0100

    added toString, getters and setters

--- src/Person.java
+++ src/Person.java
@@ -8,4 +8,33 @@ public class Person {
         this.lastName = lastName;
         this.point = 0;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " har " + point + " point";
+    }
 }
