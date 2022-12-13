98ef9b78ff10a997f2b424369d584ff0397a80d1
Teller501 <ande777j@stud.kea.dk>
2022-12-13 16:49:53 +0100

    added new person and replaced draco with neville

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,14 @@ public class Main {
         Person hermione = new Person("Hermione", "Granger");
         Person ginny = new Person("Ginny", "Weasley");
         Person draco = new Person("Draco", "Malfoy");
+        Person neville = new Person("Neville", "Longbottom");
 
         harry.addPoints(100);
         ron.addPoints(231);
         hermione.addPoints(420);
         ginny.addPoints(1000);
         draco.addPoints(215);
+        neville.addPoints(7);
 
         System.out.println(harry);
         System.out.println(ron);
@@ -24,6 +26,7 @@ public class Main {
         personer[3] = hermione;
         personer[1] = ginny;
         personer[4] = draco;
+        personer[4] = neville;
 
         System.out.println(Arrays.toString(personer));
 
