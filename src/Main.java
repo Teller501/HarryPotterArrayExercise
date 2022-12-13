3517ad56a48fed3ad12c7eeb9bb6cb51f3a6978d
Teller501 <ande777j@stud.kea.dk>
2022-12-13 16:48:36 +0100

    added new person

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
         Person ron = new Person("Ron", "Weasley");
         Person hermione = new Person("Hermione", "Granger");
         Person ginny = new Person("Ginny", "Weasley");
+        Person draco = new Person("Draco", "Malfoy");
 
         harry.addPoints(100);
         ron.addPoints(231);
         hermione.addPoints(420);
         ginny.addPoints(1000);
+        draco.addPoints(215);
 
         System.out.println(harry);
         System.out.println(ron);
@@ -20,6 +22,8 @@ public class Main {
         personer[0] = harry;
         personer[2] = ron;
         personer[3] = hermione;
+        personer[1] = ginny;
+        personer[4] = draco;
 
         System.out.println(Arrays.toString(personer));
 
