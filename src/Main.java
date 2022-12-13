007fba58dfb14a4ae19a89db447309ba430f8e94
Teller501 <ande777j@stud.kea.dk>
2022-12-13 16:47:49 +0100

    changed array to have a size of 5, and added extra person

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,21 @@ public class Main {
         Person harry = new Person("Harry", "Potter");
         Person ron = new Person("Ron", "Weasley");
         Person hermione = new Person("Hermione", "Granger");
+        Person ginny = new Person("Ginny", "Weasley");
 
         harry.addPoints(100);
         ron.addPoints(231);
         hermione.addPoints(420);
+        ginny.addPoints(1000);
 
         System.out.println(harry);
         System.out.println(ron);
         System.out.println(hermione);
 
-        Person[] personer = {harry,ron,hermione}; //Array of Person objects
+        Person[] personer = new Person[5]; //Array of Person objects
+        personer[0] = harry;
+        personer[2] = ron;
+        personer[3] = hermione;
 
         System.out.println(Arrays.toString(personer));
 
